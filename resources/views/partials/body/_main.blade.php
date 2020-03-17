039f6372a4af70e3f856b90ae39cfa9013970b5f
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-17 18:38:32 +0100

    completato collegamento per ottenere in stampa gli studenti

--- resources/views/partials/body/_main.blade.php
+++ resources/views/partials/body/_main.blade.php
@@ -1,3 +1,22 @@
-<main>
-  
+<main class="main">
+  <section class="main__students">
+    @foreach($students as $student)
+      <div class="main__students__student-card">
+        <div class="main__students__student-card__primary-info">
+          <img src="{{$student['img']}}" alt="immagine profilo studente">
+          <h3>
+            {{$student['name']}} <span>({{$student['eta']}} anni)</span>
+          </h3>
+          <p>
+            Lavora in {{$student['company']}} come {{$student['job']}}
+          </p>
+        </div>
+        <div class="main__students__student-card__secondary-info">
+          <p>
+            {{$student['description']}}
+          </p>
+        </div>
+      </div>
+    @endforeach
+  </section>
 </main>
