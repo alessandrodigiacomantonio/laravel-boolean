e466de2b849db157da93f7c22dc0e7adf9b6c3bb
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-17 20:45:19 +0100

    cards studenti

--- resources/views/partials/body/_main.blade.php
+++ resources/views/partials/body/_main.blade.php
@@ -3,13 +3,17 @@
     @foreach($students as $student)
       <div class="main__students__student-card">
         <div class="main__students__student-card__primary-info">
-          <img src="{{$student['img']}}" alt="immagine profilo studente">
-          <h3>
-            {{$student['name']}} <span>({{$student['eta']}} anni)</span>
-          </h3>
-          <p>
-            Lavora in {{$student['company']}} come {{$student['job']}}
-          </p>
+          <div class="student-card__primary-info__image">
+            <img src="{{$student['img']}}" alt="immagine profilo studente">
+          </div>
+          <div class="student-card__primary-info__text">
+            <h3>
+              {{$student['name']}} <span>({{$student['eta']}} anni)</span>
+            </h3>
+            <p>
+              Lavora in {{$student['company']}} come {{$student['job']}}
+            </p>
+          </div>
         </div>
         <div class="main__students__student-card__secondary-info">
           <p>
