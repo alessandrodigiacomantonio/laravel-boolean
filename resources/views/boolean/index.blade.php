c55048be6f082218cce862c3c9385730b62e1621
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-18 18:42:12 +0100

    aggiunta select per età (solo layout, non funzione)

--- resources/views/boolean/index.blade.php
+++ resources/views/boolean/index.blade.php
@@ -1,1 +1,35 @@
 @extends('layout')
+@section('main')
+  @if(count($students)>1)
+    <select class="main__select-by-years">
+      <option value="all">Tutti gli Studenti</option>
+      <option value="under_30">Studenti sotto i 30 anni</option>
+      <option value="between_30_50">Studenti tra i 30 e i 50 anni</option>
+      <option value="over_50">Studenti sopra i 50 anni</option>
+    </select>
+  @endif
+  <section class="main__students">
+    @foreach($students as $student)
+      <a class="main__students__student-card" href="{{ route('boolean.getStudentFromName', ['slug'=>$student['slug']]) }}">
+        <div class="main__students__student-card__primary-info">
+          <div class="student-card__primary-info__image">
+            <img src="{{$student['img']}}" alt="immagine profilo studente">
+          </div>
+          <div class="student-card__primary-info__text">
+            <h3>
+              {{$student['name']}} <span>({{$student['eta']}} anni)</span>
+            </h3>
+            <p>
+              Lavora in {{$student['company']}} come {{$student['job']}}
+            </p>
+          </div>
+        </div>
+        <div class="main__students__student-card__secondary-info">
+          <p>
+            {{$student['description']}}
+          </p>
+        </div>
+      </a>
+    @endforeach
+  </section>
+@endsection
--- resources/views/partials/body/_main.blade.php
+++ resources/views/partials/body/_main.blade.php
@@ -1,26 +1,3 @@
 <main class="main">
-  <section class="main__students">
-    @foreach($students as $student)
-      <a class="main__students__student-card" href="{{ route('boolean.getStudentFromName', ['slug'=>$student['slug']]) }}">
-        <div class="main__students__student-card__primary-info">
-          <div class="student-card__primary-info__image">
-            <img src="{{$student['img']}}" alt="immagine profilo studente">
-          </div>
-          <div class="student-card__primary-info__text">
-            <h3>
-              {{$student['name']}} <span>({{$student['eta']}} anni)</span>
-            </h3>
-            <p>
-              Lavora in {{$student['company']}} come {{$student['job']}}
-            </p>
-          </div>
-        </div>
-        <div class="main__students__student-card__secondary-info">
-          <p>
-            {{$student['description']}}
-          </p>
-        </div>
-      </a>
-    @endforeach
-  </section>
+@yield('main')
 </main>
