45fc70f81ced006b4057aa5335072405fab440e5
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-03-18 18:50:47 +0100

    aggiunto un po' di style dinamico alle cards

--- resources/views/partials/body/_footer.blade.php
+++ resources/views/partials/body/_footer.blade.php
@@ -1,3 +1,7 @@
 <footer>
-  
+  {{-- ↓ script aggiunto per evitare comportamento indesiderato delle transition su chrome ↓ --}}
+  <script type="text/javascript">
+
+  </script>
+  {{-- ↑ script aggiunto per evitare comportamento indesiderato delle transition su chrome ↑ --}}
 </footer>
